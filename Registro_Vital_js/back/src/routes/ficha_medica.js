dfcccd8e20b3a8e4870f341ab8d95d0126aea198
alvesxr <reccoalves10@gmail.com>
2025-11-10 20:03:05 -0300

    Rotas usuario e ficha_medica

---
+++ Registro_Vital_js/back/src/routes/ficha_medica.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  createFichaMedica,
+  getFichasMedicas,
+  getFichaMedicaById,
+  updateFichaMedica,
+  deleteFichaMedica,
+} from "../controllers/ficha_medica.js";
+
+const router = Router();
+
+router.post("/", createFichaMedica);
+router.get("/", getFichasMedicas);
+router.get("/:id", getFichaMedicaById);
+router.put("/:id", updateFichaMedica);
+router.delete("/:id", deleteFichaMedica);
+
+export default router;
