12cda26343dd84938188277146f865ce5af71519
MatheusMaiaRangel <matheusmaiarangel@gmail.com>
2025-11-15 23:07:56 -0300

    Adiciona confirmação de logout com alerta

--- Registro_Vital_js/front/src/components/botoes/bt_sair.jsx
+++ Registro_Vital_js/front/src/components/botoes/bt_sair.jsx
@@ -1,8 +1,32 @@
 import { clearAuthToken } from "../../utils/auth.js";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function BotaoSair() {
-	const handleLogout = () => {
-		clearAuthToken();
+	const navigate = useNavigate();
+
+	const handleLogout = async () => {
+		const result = await Swal.fire({
+			title: "Tem certeza que deseja sair?",
+			text: "Você terá que entrar novamente!",
+			icon: "warning",
+			showCancelButton: true,
+			cancelButtonColor: "#d33",
+			cancelButtonText: "Não",
+			confirmButtonColor: "#27B480",
+			confirmButtonText: "Sim",
+			reverseButtons: true,
+		});
+
+		if (result.isConfirmed) {
+			clearAuthToken();
+			await Swal.fire({
+				title: "Saiu!",
+				text: "Você saiu da sua conta.",
+				icon: "success",
+			});
+			navigate("/");
+		}
 	};
 
 	return (
